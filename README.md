ff985a949a8a58b189779617ecc7713ef7ccc601
Jeremy Cleland <jeremy@clelandco.com>
2022-12-12 18:50:59 -0500

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # odd-duck
+
 Code 201 | Lab Class 11 | Lab: Class 11
+
+[GitHub Repo](https://github.com/Jeremy-Cleland/odd-duck)
+
+[GitHub Pages]()
---
+++ js/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+// ******* GLOBALS *******
+
+
+//  ****** DOM WINDOWS *******
+
+
+
+// ***** CONSTRUCTOR FUNCTION ******
+
+
+
+// ***** HELPER FUNCTIONS / UTILITIES *****
+
+
+
+// **** EVENT HANDLERS *****
+
+
+
+// **** EXECUTABLE CODE *****
